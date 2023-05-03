55d032068ded09796c71915b9214469c28d27e81
Ademheyar <ademheyar@gmail.com>
2023-05-03 17:15:26 -0400

    createing vertical and horizontal scrollbars

--- main.py
+++ main.py
@@ -12,42 +12,113 @@ class App(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.main_dir_button = tk.Button(self, text='Select main directory', command=self.select_main_dir)
-        self.main_dir_button.pack()
+        self.c_dir_button = tk.Button(self, text='Select C directory', command=self.select_c_dir)
+        self.c_dir_button.pack()
+        self.h_dir_button = tk.Button(self, text='Select header directory', command=self.select_h_dir)
+        self.h_dir_button.pack()
 
-    def select_main_dir(self):
-        self.main_dir = filedialog.askdirectory()
-        self.main_dir_button.destroy()
+    def select_c_dir(self):
+        self.c_dir = filedialog.askdirectory()
+        self.c_dir_button.destroy()
+        self.draw_files()
+
+    def select_h_dir(self):
+        self.h_dir = filedialog.askdirectory()
+        self.h_dir_button.destroy()
         self.draw_files()
 
     def draw_files(self):
-        os.chdir(self.main_dir)
+        c_files = []
+        h_files = []
+        for dirpath, dirnames, filenames in os.walk(self.c_dir):
+            for filename in filenames:
+                if filename.endswith('.c'):
+                    c_files.append(os.path.join(dirpath, filename))
+        for dirpath, dirnames, filenames in os.walk(self.h_dir):
+            for filename in filenames:
+                if filename.endswith('.h'):
+                    h_files.append(os.path.join(dirpath, filename))
 
-        c_files = [f for f in os.listdir('.') if f.endswith('.c')]
-        h_files = [f for f in os.listdir('.') if f.endswith('.h')]
+        # Create a new Frame widget to hold the canvas and scrollbars
+        canvas_frame = tk.Frame(self)
+        canvas_frame.pack(fill=tk.BOTH, expand=True)
 
-        # Draw .c files
-        c_coords = {}
+        # Create a new Canvas widget inside the canvas frame
+        self.canvas = tk.Canvas(canvas_frame, bg='white')
+        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        # Create new scrollbar widgets attached to the canvas frame
+        y_scrollbar = tk.Scrollbar(canvas_frame, orient=tk.VERTICAL, command=self.canvas.yview)
+        y_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        x_scrollbar = tk.Scrollbar(self, orient=tk.HORIZONTAL, command=self.canvas.xview)
+        x_scrollbar.pack(side=tk.BOTTOM, fill=tk.X)
+        self.canvas.config(yscrollcommand=y_scrollbar.set, xscrollcommand=x_scrollbar.set)
+
+        # Draw files
+        coords = {}
+        max_width = self.canvas.winfo_width() - 200
+        max_height = self.canvas.winfo_height() - 200
+        bbox_widths = []
+        bbox_heights = []
         for i, c_file in enumerate(c_files):
-            x = self.canvas.winfo_width() / (len(c_files) + 1) * (i + 1)
-            y = self.canvas.winfo_height() * 0.25
-            c_coords[c_file] = (x, y)
-            self.canvas.create_rectangle(x - 50, y - 25, x + 50, y + 25, fill='lightblue')
+            bbox = self.canvas.bbox(self.canvas.create_text(0, 0, text=c_file))
+            bbox_widths.append(bbox[2] - bbox[0])
+            bbox_heights.append(bbox[3] - bbox[1])
+        bbox_width = max(bbox_widths)
+        bbox_height = max(bbox_heights)
+        grid_width = max_width // bbox_width
+        grid_height = max_height // bbox_height
+        for i, c_file in enumerate(c_files):
+            row = i // grid_width
+            col = i % grid_width
+            x = col * bbox_width + 100
+            y = row * bbox_height + 100
+            coords[c_file] = (x, y)
+            self.canvas.create_rectangle(x - bbox_width/2, y - bbox_height/2, x + bbox_width/2, y + bbox_height/2, fill='lightblue')
             self.canvas.create_text(x, y, text=c_file)
 
-        # Draw .h files
-        h_coords = {}
         for i, h_file in enumerate(h_files):
-            x = self.canvas.winfo_width() / (len(h_files) + 1) * (i + 1)
-            y = self.canvas.winfo_height() * 0.75
-            h_coords[h_file] = (x, y)
-            self.canvas.create_rectangle(x - 50, y - 25, x + 50, y + 25, fill='lightgreen')
+            row = (i + len(c_files)) // grid_width
+            col = (i + len(c_files)) % grid_width
+            x = col * bbox_width + 100
+            y = row * bbox_height + 100
+            coords[h_file] = (x, y)
+            self.canvas.create_rectangle(x - bbox_width/2, y - bbox_height/2, x + bbox_width/2, y + bbox_height/2, fill='lightgreen')
             self.canvas.create_text(x, y, text=h_file)
 
-        # Draw connections between .c and .h files
-        for c_file, h_files in self.get_relationships(c_files).items():
-            for h_file in h_files:
-                self.canvas.create_line(c_coords[c_file], h_coords[h_file], fill='gray')
+        for c_file in c_files:
+            with open(c_file, 'r') as f:
+                contents = f.read()
+                for h_file in h_files:
+                    if h_file in contents:
+                        start = coords[c_file]
+                        end = coords[h_file]
+                        self.canvas.create_line(start[0], start[1], end[0], end[1])
+
+        # Set the scrollable region of the canvas to the size of the files
+        # Set the scrollable region of the canvas to the size of the files
+        canvas.config(scrollregion=canvas.bbox("all"))
+
+        # Create a vertical scrollbar that will scroll the canvas
+        scrollbar = tk.Scrollbar(window, orient="vertical", command=canvas.yview)
+        scrollbar.pack(side="right", fill="y")
+
+        # Configure the canvas to use the scrollbar
+        canvas.configure(yscrollcommand=scrollbar.set)
+
+        # Bind the canvas to handle resizing
+        canvas.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+
+        # Create a frame inside the canvas to hold the files
+        file_frame = tk.Frame(canvas)
+        canvas.create_window((0, 0), window=file_frame, anchor="nw")
+
+        # Add the files to the frame
+        for i, file in enumerate(files):
+            file_label = tk.Label(file_frame, text=file)
+            file_label.pack(pady=10, padx=20)
+
+
 
     def get_relationships(self, c_files):
         relationships = {}
@@ -75,3 +146,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
